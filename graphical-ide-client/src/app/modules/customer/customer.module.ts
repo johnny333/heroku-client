f54dfed6d9cbd8185ce7822ca57b9eedba870e09
Tomasz Jodko <tomaszjodko2@gmail.com>
2017-01-19 18:09:19 +0100

    field component

--- graphical-ide-client/src/app/modules/customer/customer.module.ts
+++ graphical-ide-client/src/app/modules/customer/customer.module.ts
@@ -7,13 +7,16 @@ import { CustomMenuComponent } from './components/custom-menu/custom-menu.compon
 import { ProjectButtonComponent } from './components/project-button/project-button.component';
 import { ProjectViewPageComponent } from './components/project-view-page/project-view-page.component';
 import { ProfileViewPageComponent } from './components/profile-view-page/profile-view-page.component';
+import { FieldComponent } from './components/field/field.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forRoot(customerRoutes)
   ],
-  declarations: [CustomerDashboardComponent, CustomMenuComponent, ProjectButtonComponent, ProjectViewPageComponent, ProfileViewPageComponent]
+  declarations: [CustomerDashboardComponent, CustomMenuComponent, ProjectButtonComponent, ProjectViewPageComponent, ProfileViewPageComponent, FieldComponent]
 })
 export class CustomerModule {
 }
